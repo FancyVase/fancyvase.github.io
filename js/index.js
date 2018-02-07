72bed8a33b241c6b162532a063992f025c49c442
Hiram Moncivais <mm.hiram@gmail.com>
2018-02-07 01:11:46 -0500

    Made index mobile friendly

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
 $(function() {
 
   $('.link a').mouseout(function() {
-    $(this).siblings('.shape').animate({opacity: 0}, 250);
+    $(this).siblings('img').animate({opacity: 0}, 250);
   });
 
   $('.link a').mouseenter(function() {
-    $(this).siblings('.shape').animate({opacity: 1}, 250);
+    $(this).siblings('img').animate({opacity: 1}, 250);
   });
 });
